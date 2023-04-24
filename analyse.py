4f2c07750d360abc882c520d295d0bb76b0e34c0
Lukas Bürgi <buergi.lukas@gmail.com>
2023-04-24 16:50:26 +0200

    now produces csv file ready to be analysed with other tools

--- analyse.py
+++ analyse.py
@@ -7,7 +7,7 @@ import os
 import re
 import pandas
 
-dbob_years = [2016, 2018, 2019, 2021, 2022]
+dbob_years = [2014, 2015, 2016, 2018, 2019, 2021, 2022]
 
 dbob = {}
 with open('dbob.csv', 'r') as f:
@@ -25,7 +25,7 @@ with open('dbob.csv', 'r') as f:
       if(row[str(y) + ' Don’t Bank on the Bomb'] != ""):
         dbob[row['Name wie von SEC verwendet']]['dbob_to'] = y * 10000 + 1231
       
-
+results = ["Datum", "CH-Firma investiert in...", "Firma mit diesem CIK", "und diesem Namen", "Wert in USD"]
 for filename in os.listdir('data/13fs/'):
   with open("data/13fs/" + filename, 'r') as f:
     submission = BeautifulSoup(f, 'xml')
@@ -33,6 +33,7 @@ for filename in os.listdir('data/13fs/'):
       is_form13fhr = str(submission.find_all('TYPE')[0])[:12] == '<TYPE>13F-HR'
     except:
       print("Parsing error on file " + filename + ", hoping it's not the type of interest anyway.")
+      #TODO check this hopeful assumption is correct
       continue
     if(is_form13fhr):
       sec_header = str(submission.find('ACCEPTANCE-DATETIME'))
@@ -44,5 +45,9 @@ for filename in os.listdir('data/13fs/'):
         name = line.find('nameOfIssuer').text
         value = int(line.find('value').text)
         info = [date, nameCH, cik, name, value]
+        # TODO check by CIK instead of name
         if(name in dbob and dbob[name]['dbob_from'] <= date and dbob[name]['dbob_to'] >= date):
-          print(info)
+          results += info
+
+with open('results.txt', 'w', newline='') as f:
+  csv.writer(f).writerows(results)
