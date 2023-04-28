30af51200a9a973d145ad6a247d6b43c500157c0
Lukas Bürgi <buergi.lukas@gmail.com>
2023-04-28 14:48:23 +0200

    added translations (incomplete)

--- plot.py
+++ plot.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 df = pandas.read_csv('results.csv', parse_dates=[0], date_format='%Y%m%d')
 
 df2 = df.groupby(['Atombombenhersteller', 'Datum'])['Wert in USD'].sum().unstack().transpose()
+df2 = df2.fillna(0)
 
 df = df.groupby(['CH-Firma', 'Datum'])['Wert in USD'].sum().unstack().transpose()
 df = df.fillna(0)
@@ -29,12 +30,13 @@ df['Compagnie Lombard, Odier SCA'] += df['Compagnie Lombard Odier SCmA']
 df = df.rename(columns={'Compagnie Lombard, Odier SCA': 'Compagnie Lombard, Odier'})
 del df['Compagnie Lombard Odier SCmA']
 
-for c in ['GVO Asset Management Ltd', 'Jabre Capital Partners S.A.', 'Compagnie Lombard, Odier', 'Compagnie Odier SCA', 'Edmond de Rothschild', 'Freemont Management S.A.', 'GAM Holding AG', 'ARGENTIERE CAPITAL AG', 'Ameliora Wealth Management Ltd.', 'Banque Cantonale Vaudoise', 'Bellecapital International Ltd.']:
-  df['Andere'] = df[c]
-  del df[c]
-
 df['Andere'] = df['Vontobel Holding Ltd.']
 del df['Vontobel Holding Ltd.']
+for c in ['GVO Asset Management Ltd', 'Jabre Capital Partners S.A.', 'Compagnie Lombard, Odier', 'Compagnie Odier SCA', 'Edmond de Rothschild', 'Freemont Management S.A.', 'GAM Holding AG', 'ARGENTIERE CAPITAL AG', 'Ameliora Wealth Management Ltd.', 'Banque Cantonale Vaudoise', 'Bellecapital International Ltd.']:
+  df['Andere'] += df[c]
+  del df[c]
+
+
 df['Andere'] += df['Vontobel Swiss Wealth Advisors AG']
 del df['Vontobel Swiss Wealth Advisors AG']
 df['Andere'] += df['Zurich Insurance Group Ltd/FI']
@@ -50,13 +52,21 @@ del df['LFA - Lugano Financial Advisors SA']
 df['Andere'] += df['Lombard Odier Asset Management (Switzerland) SA']
 del df['Lombard Odier Asset Management (Switzerland) SA']
 
-df.plot(kind='area', colormap='Greys')
+df.plot(kind='area', colormap='Greys', xlabel="Datum", ylabel="Milliarden USD")
 plt.savefig('schweizerUnternehmen.pdf')  # saves the current figure
 
+df = df.rename(columns={'Andere': 'Autres'})
+df.plot(kind='area', colormap='Greys', xlabel="Date", ylabel="Milliards de USD")
+plt.savefig('entreprisesSuisses.pdf')  # saves the current figure
 
-for c in ['AECOM', 'AEROJET ROCKETDYNE HLDGS INC', 'BWX TECHNOLOGIES INC', 'FLUOR CORP NEW', 'HUNTINGTON INGALLS INDS INC', 'JACOBS SOLUTIONS INC', 'MOOG INC', 'TEXTRON INC']:
-  df2['Andere'] = df2[c]
+df2['Andere'] = df2['AECOM']
+del df2['AECOM']
+for c in ['AEROJET ROCKETDYNE HLDGS INC', 'BWX TECHNOLOGIES INC', 'FLUOR CORP NEW', 'HUNTINGTON INGALLS INDS INC', 'JACOBS SOLUTIONS INC', 'MOOG INC', 'TEXTRON INC']:
+  df2['Andere'] += df2[c]
   del df2[c]
 
-df2.plot(kind='area', colormap='Greys')
+df2.plot(kind='area', colormap='Greys', xlabel="Datum", ylabel="Milliarden USD")
 plt.savefig('atombombenhersteller.pdf')  # saves the current figure
+
+df2.plot(kind='area', colormap='Greys', xlabel="Date", ylabel="Milliards de USD")
+plt.savefig('fabricant darmes nucleaires.pdf')  # saves the current figure
